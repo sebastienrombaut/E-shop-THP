d0e49958152fe7b7634687999f0cbe4db5c54db3
sebastienrombaut <sebastien.rombaut@hotmail.fr>
2018-03-05 07:59:33 +0100

    Add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+
   get 'static_pages/home'
 
   root 'static_pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
