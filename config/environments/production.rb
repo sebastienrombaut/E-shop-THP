8da236692e472b26b3c183796a2b4a6ace4ea0fa
Leila Chaouih <leila_06@hotmail.com>
2018-03-06 17:43:40 +0800

    fix heroku errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,8 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif] 
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
@@ -28,7 +30,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
