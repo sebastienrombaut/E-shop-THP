85e5ce7d6c4255254cd210ff7354940ec313030c
« SophieTochon <sophie.tochon@gmail.com>
2018-03-08 01:33:58 +0100

    Fix aws

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,9 @@
+class UserMailer < ApplicationMailer
+  default from: 'notifications@example.com'
+ 
+  def welcome_email(user)
+    @user = user
+    @url  = 'https://eshop-bali.herokuapp.com/'
+    mail(to: @user.email, subject: 'From Bali 🌴 with Love ❤️')
+  end
+end
