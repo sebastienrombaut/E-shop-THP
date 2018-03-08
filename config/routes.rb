7346bb5c0f3c5ea367e232989f1261d2c4392c25
sebastienrombaut <sebastien.rombaut@hotmail.fr>
2018-03-08 07:24:57 +0100

    update

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'orders/new'
   post 'orders/new' #pour faire fonctionner stripe
 
+  get 'static_pages/about'
+
   resources :carts, only: [:index, :remove, :show] do #route qui permet de créer un id dans l'url carts/remove ce qui permet de trouver le bon item à supprimer
   	post 'carts/remove', on: :member
   end
