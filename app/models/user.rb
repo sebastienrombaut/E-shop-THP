40b98d953f19df75c72739472d00108402b4a10e
sebastienrombaut <sebastien.rombaut@hotmail.fr>
2018-03-09 02:58:45 +0100

    Updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 
-  has_one :cart
-  has_many :orders
+  has_one :cart, dependent: :destroy
+  has_many :orders, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
